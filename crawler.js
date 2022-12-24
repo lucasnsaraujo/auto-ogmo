f349fc1211ab574218e18603742fb1612c275933
Lucas Nascimento Araujo <lucas.araujo@codenapp.com>
2022-12-24 18:49:56 -0300

    finished script to crawl data

--- crawler.js
+++ crawler.js
@@ -2,8 +2,8 @@ const puppeteer = require("puppeteer");
 const { tableParser } = require("puppeteer-table-parser");
 
 const LOGIN_PAGE_URL = "https://ogmoes.com.br/intranet//Login.aspx";
-const USER_LOGIN = "";
-const USER_PASSWORD = "";
+const USER_LOGIN = "4272";
+const USER_PASSWORD = "3233";
 
 function delay(time) {
   return new Promise(function (resolve) {
@@ -12,24 +12,25 @@ function delay(time) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(LOGIN_PAGE_URL, { waitUntil: "networkidle2" });
 
   await page.type("#txtLogin", USER_LOGIN);
   await page.type("#txtSenha", USER_PASSWORD);
 
+  // As the documentation recommends, wait for page to load after clicking on login button
   await Promise.all([
     page.click("#ImgBtnLogin"),
-    page.waitForNavigation({ waitUntil: "networkidle2" }),
+    page.waitForNavigation({ waitUntil: "domcontentloaded" }),
   ]);
 
+  // Searching for the user information on the page header
   const user = await page.evaluate(() => {
     const header = document.getElementById("lblUsuario").innerText;
-
     const name = header.split("-")[1].split("(")[0].trim();
     const company_id = header.split("-")[0].trim();
-    const company_role = header.split(" ").at(-1);
+    const company_role = header.split(" ").at(-1).replace('(', '').replace(')', '');
     const request_time = Date.now().toString();
     return {
       name,
@@ -37,22 +38,36 @@ function delay(time) {
       company_role,
       request_time,
     };
-  });
+  }); 
 
-  await page.waitForSelector("#PanelEmbarque");
+  // Searching for the iframe that has the table rendered
+  const iframeElement = await page.$("iframe[id='Iframe1']")
+  const frame = await iframeElement.contentFrame();
+  const table = await frame.$('#GridViewParedes')
 
-  const work = await page.evaluate(() => {
-    const table = Array.from(
-      document.querySelectorAll("#PanelEmbarque > table > tbody > tr")
-    ).map((item) => item.innerText);
-    return { table };
-  });
-  await delay(1);
+  // Mapping table data
+  // REMINDER: In the future, make this function more automatic, getting the headers from the table
+  // instead of manually
+  const data = await table.evaluate(() => {
+    const contentRow = document.querySelectorAll('tbody > tr:nth-child(2) > td')
+    const content =  Array.from(contentRow).map((item) => item.innerText.trim()).filter((x, index) => index >= 5)
+    return {
+      parede: content[0],
+      requi: content[1],
+      operacao: content[2],
+      turno: content[3],
+      ter: content[4],
+      funcao: content[5],
+      forma: content[6],
+      navio: content[7],
+      ber: content[8],
+      cais: content[9],
+      requisitante: content[10],
+      status: content[11]
+    }
+  })
 
-  const parsed_info = { user, work };
-
-  //console.log(parsed_info);
-  console.log(work);
+  console.log({user, data})
 
   await browser.close();
 })();
