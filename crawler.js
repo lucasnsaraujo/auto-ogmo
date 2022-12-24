c7a3f3dabe33e337556152e98e667909a682187b
Lucas Nascimento Araujo <lucas.araujo@codenapp.com>
2022-12-24 19:57:53 -0300

    turning into function

--- crawler.js
+++ crawler.js
@@ -1,17 +1,10 @@
 const puppeteer = require("puppeteer");
-const { tableParser } = require("puppeteer-table-parser");
 
 const LOGIN_PAGE_URL = "https://ogmoes.com.br/intranet//Login.aspx";
-const USER_LOGIN = "4272";
-const USER_PASSWORD = "3233";
+const USER_LOGIN = "";
+const USER_PASSWORD = "";
 
-function delay(time) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, time);
-  });
-}
-
-(async () => {
+async function getWorkData() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(LOGIN_PAGE_URL, { waitUntil: "networkidle2" });
@@ -26,7 +19,7 @@ function delay(time) {
   ]);
 
   // Searching for the user information on the page header
-  const user = await page.evaluate(() => {
+  const user = await page?.evaluate(() => {
     const header = document.getElementById("lblUsuario").innerText;
     const name = header.split("-")[1].split("(")[0].trim();
     const company_id = header.split("-")[0].trim();
@@ -42,13 +35,16 @@ function delay(time) {
 
   // Searching for the iframe that has the table rendered
   const iframeElement = await page.$("iframe[id='Iframe1']")
-  const frame = await iframeElement.contentFrame();
-  const table = await frame.$('#GridViewParedes')
+  const frame = await iframeElement?.contentFrame();
+  const table = await frame?.$('#GridViewParedes')
 
   // Mapping table data
   // REMINDER: In the future, make this function more automatic, getting the headers from the table
   // instead of manually
-  const data = await table.evaluate(() => {
+  let data;
+
+  if (table) {
+  data = await table?.evaluate(() => {
     const contentRow = document.querySelectorAll('tbody > tr:nth-child(2) > td')
     const content =  Array.from(contentRow).map((item) => item.innerText.trim()).filter((x, index) => index >= 5)
     return {
@@ -66,8 +62,13 @@ function delay(time) {
       status: content[11]
     }
   })
+} else {
+  data = null;
+}
+  await browser.close();
 
   console.log({user, data})
+  return {user, data}
+};
 
-  await browser.close();
-})();
+getWorkData();
