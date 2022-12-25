d7d7713d8ab78a9259c03acc3108a794f2f77cd2
Lucas Nascimento Araujo <lucasnsaraujo0310@gmail.com>
2022-12-25 18:36:18 -0300

    finished users repository

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,19 +1,67 @@
-import UsersRepository from '../repositories/UsersRepository.js'
+import UsersRepository from "../repositories/UsersRepository.js";
 class UserController {
-    async index(request, response) {
-        
-    }
-    async store(request, response) {
-        const data = request.body;
-        const user = await UsersRepository.create(data);
-        return response.json(user)
-    }
-    async update(request, response) {
+  async index(request, response) {}
+  async show(request, response) {}
 
+  async store(request, response) {
+    const data = request.body;
+    const isBodyValid = checkIfHasAllParameters(data);
+
+    const {
+      name,
+      company_id,
+      company_role,
+      email,
+      phone_number,
+      user_login,
+      user_password,
+    } = data;
+
+    if (isBodyValid) {
+      const user = await UsersRepository.create({
+        name,
+        company_id,
+        company_role,
+        email,
+        phone_number,
+        user_login,
+        user_password,
+      });
+      return response.status(201).json(user);
+    } else {
+      return response.status(400).json({
+        error: "Some attributes are missing",
+        required: USER_MODEL,
+      });
     }
-    async delete(request, response) {
-        
-    }
+  }
+
+  async update(request, response) {}
+  async delete(request, response) {}
 }
 
-export default new UserController;
+function checkIfHasAllParameters(data) {
+  if (
+    !data.name ||
+    !data.company_id ||
+    !data.company_role ||
+    !data.email ||
+    !data.phone_number ||
+    !data.user_login ||
+    !data.user_password
+  ) {
+    return false;
+  }
+  return true;
+}
+const USER_MODEL = [
+  "name",
+  "company_id",
+  "company_role",
+  "email",
+  "phone_number",
+  "user_login",
+  "user_password",
+];
+
+export default new UserController();
--- src/app/repositories/UsersRepository.js
+++ src/app/repositories/UsersRepository.js
@@ -1,7 +1,16 @@
 import db from "../../database/index.js";
 class UsersRepository {
-  async findAll() {}
-  async findById() {}
+  async findAll() {
+    const row = await db.query(`SELECT * FROM users`);
+    return row;
+  }
+  async findById(id) {
+    const [row] = await db.query({
+      text: `SELECT * FROM users WHERE id = $1`,
+      values: [id],
+    });
+    return row;
+  }
   async create(user) {
     const {
       name,
@@ -12,7 +21,6 @@ class UsersRepository {
       user_login,
       user_password,
     } = user;
-    console.log(user)
     const [row] = await db.query({
       text: ` 
           INSERT INTO users (name, company_id, company_role, email, phone_number, user_login, user_password)
@@ -31,8 +39,45 @@ class UsersRepository {
     });
     return row;
   }
-  async update(id, data) {}
-  async delete(id) {}
+  async update(id, data) {
+    const {
+      name,
+      company_id,
+      company_role,
+      email,
+      phone_number,
+      user_login,
+      user_password,
+    } = data;
+    const [row] = await db.query({
+      text: `INSERT INTO users (name, company_id, company_role, email, phone_number, user_login, user_password)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      WHERE id = $8
+      RETURNING *
+      `,
+      values: [
+        name,
+        company_id,
+        company_role,
+        email,
+        phone_number,
+        user_login,
+        user_password,
+        id,
+      ],
+    });
+  }
+  async delete(id) {
+    const deleteOperation = await db.query({
+      text: `
+      DELETE FROM users 
+      WHERE id = $1
+    `,
+      values: [id],
+    });
+
+    return deleteOperation;
+  }
 }
 
-export default new UsersRepository;
+export default new UsersRepository();
