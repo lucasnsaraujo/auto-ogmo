e0b7a7c733aec3a43f4ec26ab1330cec781744d0
Lucas Nascimento Araujo <lucasnsaraujo0310@gmail.com>
2022-12-27 20:39:27 -0300

    testing telegram bot

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import ns from "node-schedule";
 import { getAllUsersWorkData } from "./functions/getAllUsersWorkData.js";
 import routes from "./routes.js";
+import { bot, bot as TelegramBot } from "./services/telegram.js";
 
 const app = express();
 
@@ -27,6 +28,16 @@ if (["production", "development"].includes(CURRENT_ENV)) {
 } else {
   PORT = 1234;
 }
+
+TelegramBot.onText(/\/registrar/, (msg) => {
+  const options = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: ["Gostaria de registrar", "testando"],
+    }),
+  };
+  bot.sendMessage(msg.chat.id, "Teste", options);
+});
 
 app.listen(PORT, () => {
   console.log(
--- src/services/telegram.js
+++ src/services/telegram.js
@@ -3,13 +3,13 @@ import TelegramBot from "node-telegram-bot-api";
 const isDeployed = !!["production", "development"].includes(
   process.env.CURRENT_ENV
 );
-
-const localToken = "example";
+const localToken = "";
 const TOKEN = isDeployed ? process.env.TELEGRAM_API_TOKEN : localToken;
 
-// const bot = new TelegramBot(TOKEN, {
-//   polling: true,
-// });
+export const bot = new TelegramBot(TOKEN, {
+  polling: true,
+});
+
 export async function sendTelegramMessage(telegram_id) {
   console.log(`--> Telegram message sent to ID [${telegram_id}]`);
 }
